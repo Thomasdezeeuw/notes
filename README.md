69c83be30a699d5957626ce55225caac79812e24
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-02-16 21:02:11 +0100

    Rewrite note script
    This changes the weekly notes to be daily and allow note file for a
    subject to be opened.

--- README.md
+++ README.md
@@ -1,44 +1,80 @@
-# Notes
+# Note
 
 A simple note keeper, with the help of git.
 
 Inspired by "did.txt file" by Patrick Tran, see
-https://theptrk.com/2018/07/11/did-txt-file.
+https://theptrk.com/2018/07/11/did-txt-file. And Jethro Kuan, see
+https://blog.jethro.dev/posts/how_to_take_smart_notes_org.
 
 
 ## Usage
 
-`note` can be used in the following way.
+Note keeps two kinds of notes:
 
-```
+* daily (or fleeting) notes, and
+* subject related notes.
+
+Daily notes will be opened by default and can be used for note not related to a
+specific subject. These can later be searched through. Other notes might be
+related to a specific subject, e.g. a project your working on, these are all
+contained within a single file.
+
+
+### Opening a daily note
+
+The simplest usage of `note` is command below, which opens today's note using
+your preferred editor (see below to change the editor).
+
+```bash
 $ note
 ```
 
-Opens the notes file for the current week.
+Each first usage of the day `note` will automatically create a new note file to
+write your notes in.
+
+
+### Opening a subject specific note
+
+Notes related to the same subject are better contained within the same file. To
+open the note file related to a subject the following command can be used.
+
+```bash
+$ note $subject
+# For example:
+$ note my_project
+```
+
+### Search notes
+
+After taking a bunch of notes it might become hard to remember on which day your
+wrote what. So to aid reading notes `note` support searching notes for a
+key-words.
+
+
+```bash
+$ note search $key_word
+# For example:
+$ note search note taking
+```
+
+Under the hood this using grep or [ripgrep] (if installed) to search the notes
+and opens them using your preferred editor.
+
+[ripgrep]: https://github.com/BurntSushi/ripgrep
+
 
 
 ## Environment variables
 
 The following environment variables changes the behaviour of `note`.
 
-```
-$EDITOR
-```
+* `$EDITOR` determines what editor to use when editing a note, defaults to
+  `vim`.
+* `$NOTES_DIR` sets the directory which contains all notes, defaults to
+  `~/.notes`.
+* `$NOTES_AUTO_SYNC`, if this is set to `true`, or `1`, notes will call `git add
+  . && git sync` after each call, defaults to false.
 
-Determines what editor to use when editing a note, defaults to vim.
-
-```
-$NOTES_ROOT
-```
-
-The root directory which contains all notes, defaults to "~/.notes".
-
-```
-$NOTES_AUTO_SYNC
-```
-
-If this is set to `true`, or `1`, notes will call `git sync` after each call,
-defaults to false.
 
 
 ## License
--- note.bash
+++ note.bash
@@ -10,64 +10,150 @@
 set -eu
 
 NOTES_AUTO_SYNC=${NOTES_AUTO_SYNC:-"false"}
-NOTES_ROOT=${NOTES_ROOT:-"$HOME/.notes"}
-EDTIOR=${EDITOR:-vim}
+NOTES_DIR=${NOTES_DIR:-"$HOME/.notes"}
+EDITOR=${EDITOR:-vim}
 
-# Create the git repo, if needed.
+# Initialise the notes repo, if needed.
 setup() {
-	if [[ ! -f "$NOTES_ROOT/.git/HEAD" ]]; then
-		mkdir -p "$NOTES_ROOT"
-		git init --quiet "$NOTES_ROOT"
+	if [[ ! -f "$NOTES_DIR/.git/HEAD" ]]; then
+		git init --quiet "$NOTES_DIR"
+		# Create directory structure.
+		mkdir -p "$NOTES_DIR/daily" && touch "$NOTES_DIR/daily/.gitignore"
+		mkdir -p "$NOTES_DIR/subject" && touch "$NOTES_DIR/subject/.gitignore"
+		# Create initial commit.
+		(cd "$NOTES_DIR" &&
+			git add "daily/.gitignore" "subject/.gitignore" &&
+			git commit --quiet --message="Create directory structure" 1>/dev/null)
 	fi
 }
 
 help() {
-	echo "Usage:
-note [options]
+	echo "note [options] [<command>] [<args>...]
 
-	--help            Show this help message.
+DESCRIPTION
+    note is a simple note keeper based on your preferred editor and git, using
+    plain text files for the notes themselves and a git repo for
+    synchronisation.
+
+    By default note opens the daily note. The daily note is a note file for
+    fleeting notes and is automatically created every day the first time the
+    note command is called.
+
+    Alternatively notes can be taking about a specific subject. This will
+    contain all notes for a subject within a single file.
+
+OPTIONS
+
+    --help                Shows this help message.
+
+COMMANDS
+
+    help                  Shows this help message.
+   [daily]                Opens the daily note for today.
+   [subject] [\$name]      Opens the note on subject \$name. Note that subject
+                          command is optional, i.e. \`note subject my_project\`
+                          and \`note my_project\` are interrupted as the same
+                          command.
+    search [\$keyword...]  Search all notes, daily and subject notes, for
+                          \$keyword(s).
 
 ENVIRONMENT VARIABLES
 
-	\$EDITOR          The editor with which to edit lists, defaults to vim.
-	\$NOTES_ROOT      The root directory which contains all notes, defaults to '~/.notes'.
-	\$NOTES_AUTO_SYNC If this is set to 'true', or '1', notes will sync after each edit, defaults to false."
+    \$EDITOR               The editor with which to edit lists, defaults to vim.
+    \$NOTES_DIR            The root directory which contains all notes, defaults
+                          to '~/.notes'.
+    \$NOTES_AUTO_SYNC      If this is set to 'true', or '1', notes will sync
+                          after each edit, defaults to false.
+"
 }
 
-open() {
-	FILE="$NOTES_ROOT/$(date '+%Y.%V.md')"
+open_daily() {
+	local file="$NOTES_DIR/daily/$(date '+%Y-%m-%d').md"
+	open_file "$file" "# $(date '+%A %B %d, %Y')\n\n\n"
+}
 
-	# Setup a skeleton if the file doesn't exists.
-	if [[ ! -f "$FILE" ]]; then
-		printf "# Weekly notes" >> "$FILE"
+open_subject() {
+	local subject="$1"
+	local file=$(echo "$subject" | tr '[:upper:]' '[:lower:]')
+	file="$NOTES_DIR/subject/${subject// /_}.md"
+	open_file "$file" "# $subject\n\n\n"
+}
+
+open_file() {
+	local file="$1"
+	local heading="$2"
+
+	# Add the heading if the file doesn't exists.
+	if [[ ! -f "$file" ]]; then
+		printf "$heading" >> "$file"
 	fi
 
-	# Create a section for new notes.
-	DATE=$(date '+%A %B %d %Y %H:%M')
-	printf "\n\n## $DATE\n\n\n" >> "$FILE"
-
 	# Special editor options.
-	EDITOR_OPTS=""
+	local editor_opts=""
 	if [[ "$EDITOR" == "vim" ]]; then
 		# Go to the end of the file when opened.
-		EDITOR_OPTS="+"
+		editor_opts="+"
 	fi
 
 	# Open the users editor to allow them to edit the notes.
-	$EDITOR $EDITOR_OPTS "$FILE"
+	$EDITOR $editor_opts "$file"
+
+	sync "$file"
+}
+
+sync() {
+	# Local path to the file.
+	local file=${1/$NOTES_DIR\//}
 
 	# Commit the changes.
-	COMMIT_MSG="Update notes for $DATE"
-	(cd "$NOTES_ROOT" &&
-		git add "$FILE" &&
-		git commit --quiet --message="$COMMIT_MSG" 1>/dev/null || true)
+	local commit_msg="Update $file"
+	(cd "$NOTES_DIR" &&
+		git add "$file" &&
+		git commit --quiet --message="$commit_msg" 1>/dev/null || true)
 
 	# Automatically sync the notes, if that is required.
 	if [[ "$NOTES_AUTO_SYNC" == "true" || "$NOTES_AUTO_SYNC" == "1" ]]; then
-		(cd "$NOTES_ROOT" && git push --quiet)
+		(cd "$NOTES_DIR" && git push --quiet)
 	fi
+}
+
+search() {
+	# TODO: support multiple key words.
+	local key_word="$1"
+
+	if [[ "$EDITOR" == "vim" ]]; then
+		# Open all files at once using tabs.
+		vim -p `search_files "$key_word"`
+	else
+		# Open all files one after another.
+		for file in `search_files "$key_word"`; do
+			$EDITOR "$file"
+		done
+	fi
+}
+
+search_files() {
+	local key_word="$1"
+	# TODO: port to plain grep.
+	rg --ignore-case --files-with-matches "$key_word" "$NOTES_DIR"
 }
 
 setup
 
-#open
+case ${1:-"OPEN_DAILY"} in
+	help|--help)
+		help
+	;;
+	daily|OPEN_DAILY)
+		open_daily
+	;;
+	subject)
+		open_subject "$2"
+	;;
+	search)
+		search "$2"
+	;;
+	*)
+		open_subject "$1"
+	;;
+esac
